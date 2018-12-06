bd66d7f06a8d0e592c93f404ac91cefcde3a1787
Badrikant Soni <badrikant.soni@inmobi.com>
2018-12-06 10:41:04 +0530

    assignment

---
+++ src/main/java/com/badri/patterns/factory/usecase/assigment/Male.java
@@ -0,0 +1,11 @@
+package com.badri.patterns.factory.usecase.assigment;
+
+/**
+ * @author badrikant.soni on 06/12/18,2018
+ */
+public class Male implements Person {
+
+    public void wish(String msg) {
+        System.out.println("Male : " + msg);
+    }
+}
---
+++ src/main/java/com/badri/patterns/factory/usecase/assigment/Person.java
@@ -0,0 +1,9 @@
+package com.badri.patterns.factory.usecase.assigment;
+
+/**
+ * @author badrikant.soni on 06/12/18,2018
+ */
+public interface Person {
+
+    void wish(String msg);
+}
